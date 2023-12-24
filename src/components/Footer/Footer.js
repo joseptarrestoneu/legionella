c1108583bc246f2bc355b9d62959720783c0bea1
joseptarrestoneu <joseptarrestoneu@gmail.com>
2023-12-24 10:09:49 +0100

    Add Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import { useState, useEffect } from 'react'
+import styles from './Footer.module.css'
+
+const Footer = () => {
+
+    const [ year, setYear ] = useState()
+    useEffect(() => {
+        let actualYear = new Date().getFullYear()
+        setYear(actualYear)
+    },[])
+
+    return (
+        <footer className={styles.footer}>
+            <a href="https://joseptarrestoneu.es"><span>Made by Josep Tarrés Toneu &copy; {year}</span></a>
+        </footer>
+    )
+}
+
+export default Footer
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,13 +1,17 @@
+import Footer from '../Footer/Footer'
 import styles from './Login.module.css'
 import { useState } from 'react'
 
 const Login = () => {
 
+    // Guardar estat del Login: logejat o no logejat
     const [ logged, setIsLogged ] = useState(false)
+    // Guardar user i password
     const [ userData, setUserData ] = useState({
         username: '',
-        userpass: -1,
+        userpass: '',
     })
+    // Guardar el tipus d'error en el logejat: per usuari o password equivocat o perquè està buit
     const [ error , setError ] = useState(null)
 
     const data = {
@@ -15,11 +19,12 @@ const Login = () => {
         userpass: '1234',
     }
 
+    // Funció per actualitzar el setter del usetate de loggin
     const handleLogin = (isLogged)=> setIsLogged(isLogged)
 
+    // Handle que actua al activar el formulari del Loggin
     const handleSubmit = (event) => {
         event.preventDefault()
-        console.log(userData);
         if (userData.username === data.username && userData.userpass  === data.userpass) {
             handleLogin(true)
             setError(null)
@@ -27,12 +32,21 @@ const Login = () => {
             handleLogin(false)
             if (userData.username.trim() === '' || userData.userpass.trim() === '') {
                 setError("Si us plau, introduiu les dades correctament")
+                console.log("1");
+                
             } else {
-                setError("Usuari o password incorrected")
+                setError("Usuari o password incorrecte")
+                console.log("2");
             }
         }
+        // Resetejar el formulari
+        setUserData({
+            username: '',
+            userpass: '',
+        })
     }
 
+    // Handle per controlar els inputs
     const handleChange = (event) => {
         let value = event.target.value;
         let inputName = event.target.name;
@@ -57,8 +71,8 @@ const Login = () => {
                             <a href="#" className={styles.icon}><i className="fa-solid fa-envelope"></i></a>
                         </div>
                         <span>Utilitza el teu email com a email password</span>
-                        <input className={styles.email} type='email' name='username' placeholder='Email' onChange={handleChange}></input>
-                        <input className={styles.password} type='password' name='userpass' placeholder='Password' onChange={handleChange}></input>
+                        <input className={styles.email} type='email' name='username' placeholder='Email' onChange={handleChange} value={userData.username}></input>
+                        <input className={styles.password} type='password' name='userpass' placeholder='Password' onChange={handleChange} value={userData.userpass}></input>
                         <button type='submit'>Sign In</button>
                         <a href="#">Has oblidat el Password?</a>
                     </form>
@@ -76,6 +90,7 @@ const Login = () => {
                     </div>
                 </div>
             </div>
+            <Footer />
         </div>
     )
 }
