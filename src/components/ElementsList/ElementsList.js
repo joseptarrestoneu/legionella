1a599b4a15d2e8dc13f80617946961b050144bb3
joseptarrestoneu <joseptarrestoneu@gmail.com>
2023-12-25 18:50:33 +0100

    Update ElementList component: add map() && css file table

--- src/components/ElementsList/ElementsList.js
+++ src/components/ElementsList/ElementsList.js
@@ -1,6 +1,11 @@
 import styles from './ElementsList.module.css'
+import { elementList } from '../../services/db.js'
+import { useState } from 'react'
 
 const ElementsList = () => {
+
+  const [ elements, setElements ] = useState(elementList)
+
   return (
     <div className={styles.listElements}>
       <div className={`${styles.table} ${styles.ver3}`}>
@@ -18,89 +23,25 @@ const ElementsList = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className={styles.rows}>
-              <td className={`${styles.columns} ${styles.column1}`} >Lawrence Scott</td>
-              <td className={`${styles.columns} ${styles.column2}`} >8:00 AM</td>
-              <td className={`${styles.columns} ${styles.column3}`} >--</td>
-              <td className={`${styles.columns} ${styles.column4}`} >--</td>
-              <td className={`${styles.columns} ${styles.column5}`} >8:00 AM</td>
-              <td className={`${styles.columns} ${styles.column6}`} >--</td>
-              <td className={`${styles.columns} ${styles.column7}`} >5:00 PM</td>
-              <td className={`${styles.columns} ${styles.column8}`} >8:00 AM</td>
-            </tr>            
-            <tr className={styles.rows}>
-              <td className={`${styles.columns} ${styles.column1}`} >Jane Medina</td>
-              <td className={`${styles.columns} ${styles.column2}`} >--</td>
-              <td className={`${styles.columns} ${styles.column3}`} >5:00 PM</td>
-              <td className={`${styles.columns} ${styles.column4}`} >5:00 PM</td>
-              <td className={`${styles.columns} ${styles.column5}`} >--</td>
-              <td className={`${styles.columns} ${styles.column6}`} >9:00 AM</td>
-              <td className={`${styles.columns} ${styles.column7}`} >--</td>
-              <td className={`${styles.columns} ${styles.column8}`} >--</td>
-            </tr>
-            <tr className={styles.rows}>
-              <td className={`${styles.columns} ${styles.column1}`} >Billy Mitchell</td>
-              <td className={`${styles.columns} ${styles.column2}`} >9:00 AM</td>
-              <td className={`${styles.columns} ${styles.column3}`} >--</td>
-              <td className={`${styles.columns} ${styles.column4}`} >--</td>
-              <td className={`${styles.columns} ${styles.column5}`} >--</td>
-              <td className={`${styles.columns} ${styles.column6}`} >--</td>
-              <td className={`${styles.columns} ${styles.column7}`} >2:00 PM</td>
-              <td className={`${styles.columns} ${styles.column8}`} >8:00 AM</td>
-            </tr>
-            <tr className={styles.rows}>
-              <td className={`${styles.columns} ${styles.column1}`} >Beverly Reid</td>
-              <td className={`${styles.columns} ${styles.column2}`} >--</td>
-              <td className={`${styles.columns} ${styles.column3}`} >5:00 PM</td>
-              <td className={`${styles.columns} ${styles.column4}`} >5:00 PM</td>
-              <td className={`${styles.columns} ${styles.column5}`} >--</td>
-              <td className={`${styles.columns} ${styles.column6}`} >9:00 AM</td>
-              <td className={`${styles.columns} ${styles.column7}`} >--</td>
-              <td className={`${styles.columns} ${styles.column8}`} >--</td>
-            </tr>
-            <tr className={styles.rows}>
-              <td className={`${styles.columns} ${styles.column1}`} >Tiffany Wade</td>
-              <td className={`${styles.columns} ${styles.column2}`} >8:00 AM</td>
-              <td className={`${styles.columns} ${styles.column3}`} >--</td>
-              <td className={`${styles.columns} ${styles.column4}`} >--</td>
-              <td className={`${styles.columns} ${styles.column5}`} >8:00 AM</td>
-              <td className={`${styles.columns} ${styles.column6}`} >--</td>
-              <td className={`${styles.columns} ${styles.column7}`} >5:00 PM</td>
-              <td className={`${styles.columns} ${styles.column8}`} >8:00 AM</td>
-            </tr>
-            <tr className={styles.rows}>
-              <td className={`${styles.columns} ${styles.column1}`} >Sean Adams</td>
-              <td className={`${styles.columns} ${styles.column2}`} >--</td>
-              <td className={`${styles.columns} ${styles.column3}`} >5:00 PM</td>
-              <td className={`${styles.columns} ${styles.column4}`} >5:00 PM</td>
-              <td className={`${styles.columns} ${styles.column5}`} >--</td>
-              <td className={`${styles.columns} ${styles.column6}`} >9:00 AM</td>
-              <td className={`${styles.columns} ${styles.column7}`} >--</td>
-              <td className={`${styles.columns} ${styles.column8}`} >--</td>
-            </tr>
-            <tr className={styles.rows}>
-              <td className={`${styles.columns} ${styles.column1}`} >Rachel Simpson</td>
-              <td className={`${styles.columns} ${styles.column2}`} >9:00 AM</td>
-              <td className={`${styles.columns} ${styles.column3}`} >--</td>
-              <td className={`${styles.columns} ${styles.column4}`} >--</td>
-              <td className={`${styles.columns} ${styles.column5}`} >--</td>
-              <td className={`${styles.columns} ${styles.column6}`} >--</td>
-              <td className={`${styles.columns} ${styles.column7}`} >2:00 PM</td>
-              <td className={`${styles.columns} ${styles.column8}`} >8:00 AM</td>
-            </tr>
-            <tr className={styles.rows}>
-              <td className={`${styles.columns} ${styles.column1}`} >Mark Salazar</td>
-              <td className={`${styles.columns} ${styles.column2}`} >8:00 AM</td>
-              <td className={`${styles.columns} ${styles.column3}`} >--</td>
-              <td className={`${styles.columns} ${styles.column4}`} >--</td>
-              <td className={`${styles.columns} ${styles.column5}`} >8:00 AM</td>
-              <td className={`${styles.columns} ${styles.column6}`} >--</td>
-              <td className={`${styles.columns} ${styles.column7}`} >5:00 PM</td>
-              <td className={`${styles.columns} ${styles.column8}`} >
-                <abbr title="Editar element"><i class="fa-solid fa-pen-to-square"></i></abbr>
-                <abbr title="Eliminar element"><i class="fa-solid fa-trash" ></i></abbr>
-              </td>
-            </tr>
+            {
+              elements.map(elements => {
+                return (
+                  <tr className={styles.rows}>
+                    <td className={`${styles.columns} ${styles.column1}`} >{elements.id}</td>
+                    <td className={`${styles.columns} ${styles.column2}`} >{elements.elementReference}</td>
+                    <td className={`${styles.columns} ${styles.column3}`} >{elements.elementName}</td>
+                    <td className={`${styles.columns} ${styles.column4}`} >{elements.elementUbication}</td>
+                    <td className={`${styles.columns} ${styles.column5}`} >{elements.elementAreaId}</td>
+                    <td className={`${styles.columns} ${styles.column6}`} >{elements.elementCompanyId}</td>
+                    <td className={`${styles.columns} ${styles.column7}`} >{elements.elementActive && "Actiu" || "No actiu"}</td>
+                    <td className={`${styles.columns} ${styles.column8}`} >
+                      <i class="fa-solid fa-pen-to-square"></i>
+                      <i class="fa-solid fa-trash" ></i>
+                    </td>
+                  </tr>  
+                )
+              })
+            }
           </tbody>
         </table>
       </div>
